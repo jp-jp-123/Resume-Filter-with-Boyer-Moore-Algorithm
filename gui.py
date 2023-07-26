08f0fce94937c70f82bfaa3238004b7a4e6052d9
DevianKeno <70948471+DevianKeno@users.noreply.github.com>
2023-07-26 09:11:36 +0800

    Revert "- self.boyer_moore.parse_results() accidentally removed"
    This reverts commit 9311d12a5225d313cbcd3a7de68cf8ff58327483.

--- gui.py
+++ gui.py
@@ -30,10 +30,10 @@ class ResumeFilterApp(tk.Tk):
     def start_boyer_moore(self):
         self.boyer_moore.start()
         self.boyer_moore.set_low_level_logging(False)
+        self.boyer_moore.set_low_level_logging(True)
         self.boyer_moore.start()
-        self.boyer_moore.parse_results()
         self.boyer_moore.segregate_pdf()
-        logger.make_logfile(self.boyer_moore.filepath, enable=False)
+        logger.make_logfile(self.boyer_moore.filepath)
 
     def start(self):
         self.set_path()
